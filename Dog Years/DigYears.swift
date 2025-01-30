dba849a6e4b51b4cea6efc9b27be5193ab614cf1
Esteban Lopez 👋 <esteban.steam21@gmail.com>
2025-01-30 11:02:22 +0900

    Create DigYears.swift

---
+++ Dog Years/DigYears.swift
@@ -0,0 +1,25 @@
+// Tobi is 6 uears old
+var dogAge = 6
+// var dogAge: Double = 2.5
+
+var earlyYears = 0
+var laterYears = 0
+var humanYears = 0
+
+// let earlyYears: Double
+// let laterYears: Double
+// let humanYears: Double
+
+// The first two years years 
+earlyYears = 21
+
+// The following years
+laterYears = (dogAge - 2) * 4
+
+// Your dog's age in human years
+humanYears = earlyYears + laterYears
+
+print("My name is Tobi! Ruff Ruff, I am \(humanYears) years old in human years.")
+
+Output: 
+My name is Tobi! Ruff Ruff, I am 37 years old in human years.
