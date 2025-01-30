616dc1aeb5ec049df634fd19b1f398cc468426e4
Esteban Lopez 👋 <esteban.steam21@gmail.com>
2025-01-30 11:20:06 +0900

    Create PiggyBank.swift

---
+++ piggy bank/PiggyBank.swift
@@ -0,0 +1,17 @@
+var pesos: Double = 4
+var reais: Double = 5
+var soles: Double = 6
+
+var total: Double
+
+// Google Search on 1/30/2025:
+// 🇨🇴 Pesos conversion rate: 0.00029
+// 🇧🇷 Reais conversion rate: 0.24
+// 🇵🇪 Soles conversion rate: 0.29
+
+
+total = 0.00024 * pesos + 0.16 * reais + 0.27 * soles
+
+print("Total 🇺🇸USD = $\(total)")
+
+Output: Total 🇺🇸USD = $2.42096
