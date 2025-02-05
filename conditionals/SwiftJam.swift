66277e50c79ab192b63b4299b2cf199bd0359bce
Esteban Lopez 👋 <esteban.steam21@gmail.com>
2025-02-05 13:27:12 +0900

    Create SwiftJam.swift

---
+++ conditionals/SwiftJam.swift
@@ -0,0 +1,11 @@
+let tuneSquadPoints = 78
+let monstarsPoints = 77
+
+// Write your code below 🏀
+
+
+if tuneSquadPoints < monstarsPoints {
+  print("Monstars Win!🏀")
+} else {
+  print("TuneSquad Wins!🏀")
+}
