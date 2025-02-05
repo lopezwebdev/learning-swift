2968de49e6763a376257c125240133cd86ca04da
Esteban Lopez 👋 <esteban.steam21@gmail.com>
2025-02-05 13:28:06 +0900

    Create Languages.swift

---
+++ conditionals/Languages.swift
@@ -0,0 +1,22 @@
+// Write your code below 🗽
+
+/*
+English: EN
+Spanish: ES
+Chinese: ZH
+Russian: RU
+*/ 
+
+var abbreviation = "ES"
+
+if abbreviation == "EN" {
+  print("English")
+} else if abbreviation == "ES" {
+  print("Spanish")
+} else if abbreviation == "ZH" {
+  print("Chinese")
+} else if abbreviation == "RU" {
+  print("Russian")
+} else {
+  print("Abbreviation not found.")
+}
